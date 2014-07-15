293af192969f2bc179c98350a584de1037b91c61
Priyank <PriyankBhatt@users.noreply.github.com>
2014-07-16 00:58:00 +0530

    Checking opencv version,Tolerance
    1.Checking Version of opencv and implementing kmeans accordingly.
    2.Decrease Tolerance for better accuracy 0.8-----0.0001

--- mergeCenters.py
+++ mergeCenters.py
@@ -5,6 +5,7 @@ training database.
 """
 import numpy as np
 import cv2
+from cv2 import __version__
 import os
 import pickle
 import sys
@@ -27,9 +28,12 @@ def mergeCenters(nCenters):
 	    Center.close()
 
 	center = np.float32(center)
-	criteria = (cv2.TERM_CRITERIA_MAX_ITER, 10,0.8)
-	#Remove third parameter if you are using opencv 2.4.9 or lesser.
-	ret,label,center=cv2.kmeans(center,int(nCenters),None,criteria,50,cv2.KMEANS_PP_CENTERS)
+	criteria = (cv2.TERM_CRITERIA_MAX_ITER, 10,0.0001)
+	#Checking version of opencv..
+	if __verison__[0] == '3':
+		ret,label,center=cv2.kmeans(center,int(nCenters),None,criteria,50,cv2.KMEANS_PP_CENTERS)
+	else:
+		ret,label,center=cv2.kmeans(center,int(nCenters),criteria,50,cv2.KMEANS_PP_CENTERS)
 
 	CenterFinal = open(path+'/centerFinal.p',"wb")#: File pointer for final centers file
 	pickle.dump(center, CenterFinal)	#Dump centers to file
