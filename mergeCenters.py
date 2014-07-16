bbb5ebd7a5ca9bbca7f6235ab0c2351485db971b
Ankit Shah <shahankit2313@gmail.com>
2014-07-16 14:29:33 +0530

    Update mergeCenters.py

--- mergeCenters.py
+++ mergeCenters.py
@@ -5,7 +5,6 @@ training database.
 """
 import numpy as np
 import cv2
-from cv2 import __version__
 import os
 import pickle
 import sys
@@ -30,7 +29,7 @@ def mergeCenters(nCenters):
 	center = np.float32(center)
 	criteria = (cv2.TERM_CRITERIA_MAX_ITER, 10,0.0001)
 	#Checking version of opencv..
-	if __verison__[0] == '3':
+	if cv2.__verison__[0] == '3':
 		ret,label,center=cv2.kmeans(center,int(nCenters),None,criteria,50,cv2.KMEANS_PP_CENTERS)
 	else:
 		ret,label,center=cv2.kmeans(center,int(nCenters),criteria,50,cv2.KMEANS_PP_CENTERS)
