57912121eb1fb795e5715a6f7589e288348d32bc
Ankit Shah <shahankit2313@gmail.com>
2015-03-06 22:09:05 +0530

    updated run.sh file

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#python siftExtractor.py
-#python kMeans.py $1
-#python mergeCenters.py $1
+if [ $1 = "" ]; then
+	echo "Error: Must pass number of centers as argument"
+fi
+python siftExtractor.py
+python kMeans.py $1
+python mergeCenters.py $1
 python makeDatabase.py $1
 python svmTrain.py
 python predict.py $1
